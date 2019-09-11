990b088255e941f03c071ec23af52b3bc8e727b0
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-11 11:07:51 +0200

    Dateien zeilenweise auslesen und wiedergeben

--- dateien_lesen/index.php
+++ dateien_lesen/index.php
@@ -2,4 +2,13 @@
 $zitate = file_get_contents('zitate.txt');
 echo nl2br($zitate);
 // nl2br()  welche Zeilenumbrüche (new lines) in HTML-Zeilenumbrüche (<br />) umwandelt
+
+echo '<br><br>';
+
+
+// Dateien zeilenweise auslesen mittels file()
+$zitate2 = file('zitate.txt');
+for ($i=0; $i < count($zitate2); $i++) {
+  echo $i.": " .$zitate2[$i]. "<br><br>";
+}
 ?>
